6bee18f344121f60729a1c8952b4b7a682afa358
Mudashir <mudashirsakariyah2019@gmail.com>
2026-02-21 12:41:49 +0100

    new commit

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//Booking
+
 document.querySelector("#booking form").addEventListener("submit", async function (e) {
     e.preventDefault();
   
@@ -59,3 +61,29 @@ window.addEventListener('scroll', () => {
     }
   });
 });
+
+
+
+//Admin
+
+async function login() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const res = await fetch("https://saloon-backend-plfu.onrender.com/api/admin/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ email, password })
+  });
+
+  const data = await res.json();
+
+  if (res.ok) {
+    localStorage.setItem("admin", "true");
+    window.location.href = "dashboard.html";
+  } else {
+    document.getElementById("msg").innerText = data.error;
+  }
+};
+
+
